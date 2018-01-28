0bbe6c65ef86651c36329ce6719b6d67b3b9f59d
thraaawn <david.hilberath@gmx.de>
2018-01-28 09:37:19 +0100

    Bumped to 1.0.4

--- src/main/java/org/dave/ocsensors/OCSensors.java
+++ src/main/java/org/dave/ocsensors/OCSensors.java
@@ -18,7 +18,7 @@ import org.dave.ocsensors.proxy.CommonProxy;
 @Mod(modid = OCSensors.MODID, version = OCSensors.VERSION, dependencies = "after:*;required-after:opencomputers", acceptedMinecraftVersions = "[1.12,1.13)")
 public class OCSensors {
     public static final String MODID = "ocsensors";
-    public static final String VERSION = "1.0.3";
+    public static final String VERSION = "1.0.4";
 
     @SidedProxy(clientSide = "org.dave.ocsensors.proxy.ClientProxy", serverSide = "org.dave.ocsensors.proxy.ServerProxy")
     public static CommonProxy proxy;
